420401dbcf7cb46095993a8da83921d02407d21e
reid <reidsherman@gmail.com>
2017-11-12 10:25:13 -0800

    Seed database with test user

--- onionup/db/seeds.rb
+++ onionup/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.new(
+  email: 'fakemail2@gmail.com',
+  password: '123456'
+)
+user1.save!
+
+site1 = Site.new(
+  url:  URI.parse('http://answerstedhctbek.onion/'),
+  user_id: user1.id
+)
+
+site1.save
