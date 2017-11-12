2efbfc5a35c482070c6648e525de21f5a8c78587
reid <reidsherman@gmail.com>
2017-11-11 20:20:53 -0800

    Start testing tor tcp connection

--- onionup/makereq.rb
+++ onionup/makereq.rb
@@ -1,3 +1,11 @@
-require 'tor/http'
-require 'tor/configuration'
-require 'tor/tor_requests'
+require './tor/http'
+require './tor/configuration'
+require './tor/tor_requests'
+
+res = Tor::HTTP.get("216.58.194.196", "/", 80)
+p res.code
+p res.body
+
+res = Tor::HTTP.post('posttestserver.com', {"var" => "variable"}, '/post.php?dir=example', 80)
+p res.code
+p res.body
