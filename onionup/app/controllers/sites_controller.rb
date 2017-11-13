854bb307ae33e2670f0374472527d6f93e86c2e9
reid <reidsherman@gmail.com>
2017-11-12 18:21:07 -0800

    User creates new sites

--- onionup/app/controllers/sites_controller.rb
+++ onionup/app/controllers/sites_controller.rb
@@ -1,5 +1,6 @@
 class SitesController < ApplicationController
   def index
+    @sites = current_user.sites
   end
 
   def show
@@ -7,10 +8,30 @@ class SitesController < ApplicationController
   end
 
   def new
+
+  end
+
+  def create
+    @site = Site.new(site_params)
+    @site.user_id = current_user.id
+    if @site.save
+      redirect_to sites_url
+      return
+    else
+      flash.now[:errors] = @site.errors.full_messages
+      render :new
+      return
+    end
+    # fail
   end
 
   def delete
   end
 
+  private
+
+  def site_params
+    params.require(:site).permit(:url)
+  end
 
 end
--- onionup/config/routes.rb
+++ onionup/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
-  resources :sites, only: [:show, :index, :delete, :new]
+  resources :sites, only: [:show, :index, :create, :delete, :new]
 end
