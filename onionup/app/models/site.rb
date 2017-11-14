37a2e5a517239c3beb21ce4a8ef89d9b2c2a561c
reid <reidsherman@gmail.com>
2017-11-13 18:50:57 -0800

    Fix ping issue where error was thrown

--- onionup/app/models/site.rb
+++ onionup/app/models/site.rb
@@ -5,12 +5,17 @@ class Site < ApplicationRecord
 
   def ping
     uri = URI.parse(self.url)
-    if !uri.host.nil? && uri.path
-      Net::HTTP.SOCKSProxy('127.0.0.1', 9050).start(uri.host, uri.port) do |http|
-        # p http.get(uri.path)
-        return true
+    # if !uri.host.nil? && uri.path
+      begin
+        Net::HTTP.SOCKSProxy('127.0.0.1', 9050).start(uri.host, uri.port) do |http|
+          return true
+        end
+          # p http.get(uri.path)
+        # return true
+      rescue
+        puts "Host unreachable error"
       end
-    end
+    # end
     false
   end
 end
